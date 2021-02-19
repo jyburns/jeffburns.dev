095af02056b55a9644ea1fab673bb9f012370ec9
jyburns <jyburns95@gmail.com>
2021-02-19 21:43:01 +0000

    initial commit for new 11ty site

---
+++ .eleventy.js
@@ -0,0 +1,25 @@
+const svgContents = require("eleventy-plugin-svg-contents");
+
+module.exports = (eleventyConfig) => {
+  eleventyConfig.addPassthroughCopy('src/assets');
+  eleventyConfig.addPassthroughCopy('src/favicon.png');
+  eleventyConfig.addPassthroughCopy('src/scripts');
+  eleventyConfig.addPassthroughCopy('src/styles');
+
+  eleventyConfig.addPlugin(svgContents);
+
+  eleventyConfig.addFilter("longDate", (date) => {
+    return new Date(date).toDateString();
+  });
+  
+  return {
+    dir: {
+        input: 'src',
+        output: '_site',
+        includes: '_includes',
+    },
+
+    htmlTemplateEngine: 'njk',
+    markdownTemplateEngine: "njk"
+  };
+}
---
+++ src/scripts/boids.js
@@ -0,0 +1,5 @@
+console.log('where are the boids?');
+
+window.addEventListener('DOMContentLoaded', (event) => {
+  console.log('DOM fully loaded and parsed', event);
+});
