243e61c2f8d32f8e6e3b52277ce756f83942f022
jyburns <jyburns95@gmail.com>
2021-03-01 00:01:35 +0000

    fix safari compatibility issues

--- src/scripts/modules/quickMaths.js
+++ src/scripts/modules/quickMaths.js
@@ -1,117 +1,119 @@
 class Vector {
-  #magnitude = 0;
-  #theta = 0;
-  #dx = null;
-  #dy = null;
+  _magnitude = 0;
+  _theta = 0;
+  _dx = null;
+  _dy = null;
 
   constructor(magnitude = 0, theta = 0) {
-    this.#magnitude = magnitude,
-    this.#theta = theta;
+    this._magnitude = magnitude,
+    this._theta = theta;
   }
 
   setMagnitude(magnitude) {
-    this.#magnitude = magnitude;
-    this.#dx = null;
-    this.#dy = null;
+    this._magnitude = magnitude;
+    this._dx = null;
+    this._dy = null;
   }
 
   setTheta(theta) {
-    this.#theta = theta;
-    this.#dx = null;
-    this.#dy = null;
+    this._theta = theta;
+    this._dx = null;
+    this._dy = null;
   }
 
   get magnitude() {
-    return this.#magnitude;
+    return this._magnitude;
   }
 
   get theta() {
-    return normalizeTheta(this.#theta);
+    return normalizeTheta(this._theta);
   }
 
   get dx() {
-    if (this.#dx === null) {
-      this.#dx = Math.cos(this.#theta) * this.#magnitude;
+    if (this._dx === null) {
+      this._dx = Math.cos(this._theta) * this._magnitude;
     }
 
-    return this.#dx;
+    return this._dx;
   }
 
   get dy() {
-    if (this.#dy === null) {
-      this.#dy = Math.sin(this.#theta) * this.#magnitude;
+    if (this._dy === null) {
+      this._dy = Math.sin(this._theta) * this._magnitude;
     }
     
-    return this.#dy;
+    return this._dy;
   }
 
   scale(multiplier) {
-    this.#magnitude = this.#magnitude * multiplier;
+    this._magnitude = this._magnitude * multiplier;
+  }
+};
+
+// faux static methods
+Vector.fromCoordinates = (x, y) => {
+  let magnitude = magnitude2D(x, y);
+  let theta = Math.atan(y / x);
+
+  if (x < 0) {
+    theta += Math.PI;
   }
 
-  static fromCoordinates = (x, y) => {
-    let magnitude = magnitude2D(x, y);
-    let theta = Math.atan(y / x);
-
-    if (x < 0) {
-      theta += Math.PI;
-    }
-
-    return new Vector(magnitude, theta);
-  };
-
-  static mean = (vectorList) => {
-    if (vectorList.length === 0) {
-      return new Vector(0, 0);
-    }
-
-    let xSum = 0;
-    let ySum = 0;
-
-    vectorList.forEach(v => {
-      xSum += v.dx;
-      ySum += v.dy;
-    });
-
-    let xAvg = xSum / vectorList.length;
-    let yAvg = ySum / vectorList.length;
-
-    return Vector.fromCoordinates(xAvg, yAvg);
-  };
-
-  static sum = (vectorList) => {
-    if (vectorList.length === 0) {
-      return new Vector(0, 0);
-    }
-
-    let xSum = 0;
-    let ySum = 0;
-
-    vectorList.forEach(v => {
-      xSum += v.dx;
-      ySum += v.dy;
-    });
-
-    return Vector.fromCoordinates(xSum, ySum);
-  };
-
-  static dotProduct = (v1, v2) => {
-    return (v1.dx * v2.dx) + (v1.dy * v2.dy);
-  };
-
-  static thetaBetween = (v1, v2) => {
-    if (v1.magnitude * v2.magnitude === 0) {
-      return 0;
-    }
-
-    let directionOfRotation = 1;
-    if ((v1.theta - v2.theta > 0) && (Math.abs(v1.theta - v2.theta) < Math.PI)) {
-      directionOfRotation = -1;
-    }
-
-    return Math.acos(this.dotProduct(v1, v2) / (v1.magnitude * v2.magnitude)) * directionOfRotation;
-  };
+  return new Vector(magnitude, theta);
 };
+
+Vector.mean = (vectorList) => {
+  if (vectorList.length === 0) {
+    return new Vector(0, 0);
+  }
+
+  let xSum = 0;
+  let ySum = 0;
+
+  vectorList.forEach(v => {
+    xSum += v.dx;
+    ySum += v.dy;
+  });
+
+  let xAvg = xSum / vectorList.length;
+  let yAvg = ySum / vectorList.length;
+
+  return Vector.fromCoordinates(xAvg, yAvg);
+};
+
+Vector.sum = (vectorList) => {
+  if (vectorList.length === 0) {
+    return new Vector(0, 0);
+  }
+
+  let xSum = 0;
+  let ySum = 0;
+
+  vectorList.forEach(v => {
+    xSum += v.dx;
+    ySum += v.dy;
+  });
+
+  return Vector.fromCoordinates(xSum, ySum);
+};
+
+Vector.dotProduct = (v1, v2) => {
+  return (v1.dx * v2.dx) + (v1.dy * v2.dy);
+};
+
+Vector.thetaBetween = (v1, v2) => {
+  if (v1.magnitude * v2.magnitude === 0) {
+    return 0;
+  }
+
+  let directionOfRotation = 1;
+  if ((v1.theta - v2.theta > 0) && (Math.abs(v1.theta - v2.theta) < Math.PI)) {
+    directionOfRotation = -1;
+  }
+
+  return Math.acos(Vector.dotProduct(v1, v2) / (v1.magnitude * v2.magnitude)) * directionOfRotation;
+};
+// end faux static methods
 
 const distance2D = (x1, y1, x2, y2) => {
   return Math.sqrt((x1 - x2)**2 + (y1 - y2)**2);
